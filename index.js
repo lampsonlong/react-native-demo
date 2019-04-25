2f45b87f9b093f5524fd312a9096c58cb51d1243
jojo <394590166@qq.com>
2019-04-25 17:05:01 +0800

    debug网络请求

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
+
+// ↓↓↓↓↓↓↓↓↓↓↓↓debug网络请求用↓↓↓↓↓↓↓↓↓↓↓↓
+global.XMLHttpRequest = global.originalXMLHttpRequest ? global.originalXMLHttpRequest : global.XMLHttpRequest;
+global.FormData = global.originalFormData ? global.originalFormData : global.FormData;
+
+fetch
+// Ensure to get the lazy property
+if (window.__FETCH_SUPPORT__) {
+    // it's RNDebugger only to have
+    window.__FETCH_SUPPORT__.blob = false;
+} else {
+    global.Blob = global.originalBlob ? global.originalBlob : global.Blob;
+    global.FileReader = global.originalFileReader ? global.originalFileReader : global.FileReader;
+}
+// ↑↑↑↑↑↑↑↑↑↑↑debug网络请求用↑↑↑↑↑↑↑↑↑↑↑
